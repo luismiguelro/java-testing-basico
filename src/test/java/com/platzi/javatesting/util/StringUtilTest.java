a1b38d6fd66421eab03de4afd663baeb3a1cac3b
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-05-24 09:21:24 -0500

    creacion test unitario: lanzar excepcion error

--- src/test/java/com/platzi/javatesting/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatesting/util/StringUtilTest.java
@@ -3,23 +3,24 @@ package com.platzi.javatesting.util;
 public class StringUtilTest {
     public static void main(String[] args) {
 
-        // Prueba 1 : Manera muy manual
-        //Llamar funcion
-        String result= StringUtil.repeat("Hola",3);
-       // System.out.println(result);
+        // Prueba 1
 
+        // Comprobar mediante una funcion
+        assertEquals(StringUtil.repeat("Hola",3),"HolaHolaHola");
+
+
+        // Prueba 1.1
+
+        // Comprobar mediante una funcion
+        assertEquals(StringUtil.repeat("Hola",1),"Hola");
+
+    }
+
+
+    private static void assertEquals(String actual, String expected) {
         // comprobar resultado esperado
-        if(result.equals("HolaHolaHola")){
-            System.out.println("OK");
-        }
-
-        // Prueba 2
-        String result2= StringUtil.repeat("Hola",1);
-        // System.out.println(result2);
-
-        // comprobar resultado esperado
-        if(result2.equals("Hola")){
-            System.out.println("OK");
+        if(!actual.equals(expected)){
+            throw new RuntimeException(actual + " is not equal to expected: "+expected);
         }
     }
 }
