7b3f5131250a7e332337e28bbea5f1324e331036
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-05-24 09:52:06 -0500

    Instalacion de JUnit y test unitario

--- src/test/java/com/platzi/javatesting/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatesting/util/StringUtilTest.java
@@ -1,26 +1,27 @@
 package com.platzi.javatesting.util;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class StringUtilTest {
-    public static void main(String[] args) {
+
+    // inidicar que es un test
+    @Test
+    public void testRepeat() {
+
 
         // Prueba 1
 
-        // Comprobar mediante una funcion
-        assertEquals(StringUtil.repeat("Hola",3),"HolaHolaHola");
+        // Comprobar mediante una funcion de JUnit
+        Assert.assertEquals("HolaHolaHola",StringUtil.repeat("Hola",3));
 
 
         // Prueba 1.1
 
-        // Comprobar mediante una funcion
-        assertEquals(StringUtil.repeat("Hola",1),"Hola");
+        // Comprobar mediante una funcion de JUnit
+        Assert.assertEquals("Hola",StringUtil.repeat("Hola",1));
 
     }
 
 
-    private static void assertEquals(String actual, String expected) {
-        // comprobar resultado esperado
-        if(!actual.equals(expected)){
-            throw new RuntimeException(actual + " is not equal to expected: "+expected);
-        }
-    }
+
 }
