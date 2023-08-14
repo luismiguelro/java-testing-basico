4a0b0700c29e47191514dd4c22efcfa985939114
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-08-14 05:29:57 -0500

    [add] Practica Mockito: tests con diferentes opciones y refactor

--- src/test/java/com/platzi/javatesting/project_extra/GameTest.java
+++ src/test/java/com/platzi/javatesting/project_extra/GameTest.java
@@ -1,6 +1,7 @@
 package com.platzi.javatesting.project_extra;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -18,6 +19,9 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class GameTest {
 
+    public static final int OPTION_ROCK = 0;
+    public static final int OPTION_PAPER = 1;
+    public static final int OPTION_SCISSORS = 2;
     //indicar clase principal
     @InjectMocks
     private Game game;
@@ -28,17 +32,23 @@ public class GameTest {
 
     @Mock
     Random random;
+
+    private ByteArrayOutputStream out;
+
+    @Before
+    public void setUp(){
+        //obtener lo que se esta imprimiendo en consola
+        out = new ByteArrayOutputStream();
+
+        // indicar objeto a utilizar como consola
+        System.setOut(new PrintStream(out));
+    }
     @Test
     public void when_write_quit_then_exit_game(){
 
         //simular con mockito
         when(scanner.nextLine()).thenReturn("Quit");
 
-        //obtener lo que se esta imprimiendo en consola
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-        // indicar objeto a utilizar como consola
-        System.setOut(new PrintStream(out));
 
         // crear un juego
         game.play();
@@ -50,23 +60,92 @@ public class GameTest {
     @Test
     public void when_chose_rock_then_beats_scissors(){
 
-        //simular con mockito: rock
-        when(scanner.nextLine()).thenReturn("Rock ").thenReturn("Quit");
-        // simular la respuesta random
-        when(random.nextInt(3)).thenReturn(2);
+        //simular con mockito, la seleccion del usuario
+        when(scanner.nextLine()).thenReturn("Rock").thenReturn("Quit");
+        // simular la respuesta random del pc
+        when(random.nextInt(3)).thenReturn(OPTION_SCISSORS);
 
-        //obtener lo que se esta imprimiendo en consola
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-        // indicar objeto a utilizar como consola
-        System.setOut(new PrintStream(out));
 
         // crear un juego
         game.play();
 
-        // comparar parte del  texto
-       Assert.assertTrue(out.toString().contains("Let's play Rock"));
+        //pruebas
+       Assert.assertTrue(out.toString().contains("Computer chose scissors"));
+       Assert.assertTrue(out.toString().contains("wins:1"));
+
     }
 
+    @Test
+    public void when_chose_scissors_then_beats_paper(){
+
+        //simular con mockito, la seleccion del usuario
+        when(scanner.nextLine()).thenReturn("scissors").thenReturn("Quit");
+        // simular la respuesta random del pc
+        when(random.nextInt(3)).thenReturn(OPTION_PAPER);
+
+
+        // crear un juego
+        game.play();
+
+        //pruebas
+        Assert.assertTrue(out.toString().contains("Computer chose paper"));
+        Assert.assertTrue(out.toString().contains("wins:1"));
+
+    }
+
+    @Test
+    public void when_chose_paper_then_beats_rock(){
+
+        //simular con mockito, la seleccion del usuario
+        when(scanner.nextLine()).thenReturn("paper").thenReturn("Quit");
+        // simular la respuesta random del pc
+        when(random.nextInt(3)).thenReturn(OPTION_ROCK);
+
+
+        // crear un juego
+        game.play();
+
+        //pruebas
+        Assert.assertTrue(out.toString().contains("Computer chose rock"));
+        Assert.assertTrue(out.toString().contains("wins:1"));
+
+    }
+
+    @Test
+    public void when_both_choose_rock_then_tie(){
+
+        //simular con mockito, la seleccion del usuario
+        when(scanner.nextLine()).thenReturn("rock").thenReturn("Quit");
+        // simular la respuesta random del pc
+        when(random.nextInt(3)).thenReturn(OPTION_ROCK);
+
+
+        // crear un juego
+        game.play();
+
+        //pruebas
+        Assert.assertTrue(out.toString().contains("Computer chose rock"));
+        Assert.assertTrue(out.toString().contains("ties:1"));
+
+    }
+
+    @Test
+    public void when_choose_rock_and_computer_choose_paper_then_lose(){
+
+        //simular con mockito, la seleccion del usuario
+        when(scanner.nextLine()).thenReturn("rock").thenReturn("Quit");
+        // simular la respuesta random del pc
+        when(random.nextInt(3)).thenReturn(OPTION_PAPER);
+
+
+
+        // crear un juego
+        game.play();
+
+        //pruebas
+        Assert.assertTrue(out.toString().contains("Computer chose paper"));
+        Assert.assertTrue(out.toString().contains("loses:1"));
+
+    }
 }
 
